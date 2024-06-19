d75acc3e6f2db3a1845bc41e66a678fa85fdc4f6
Patrick Bassem Ramy <patrick.bassemrs@gmail.com>
2024-06-19 16:21:01 +0300

    Job-Circle/jcproduct #3 added 'seperate_post_from_whatsapp_bulk_text' function

--- app/services/post_parser.py
+++ app/services/post_parser.py
@@ -11,7 +11,7 @@ def extract_contact_info(text):
 
     Returns
     -------
-    list with 'phone number(s)' and 'email(s)'
+    dictionary with 'phone number(s)' and 'email(s)'
     """
     phone_patterns = [
         r"[\d٠-٩]{11}",  # Matches 11 digits (ASCII or Arabic)
@@ -28,3 +28,44 @@ def extract_contact_info(text):
     matches_email.extend(re.findall(email_pattern, text))
     contact = {"mobile": matches_phone, "email": matches_email}
     return contact
+
+
+def seperate_posts_from_whatsapp_bulk_text(bulk):
+    """
+    Add WhatApp text bulk.
+
+    Parameters
+    ----------
+    bulk: string
+
+    Returns
+    -------
+    list (of strings) containing each message as an element excluding
+    repeated format
+
+    Example
+    -------
+    ['message 1',
+    'message 2',
+    'message 3']
+    """
+    # These patterns extract the message right after the repeated format exclusively
+    unsaved_ctc_sep_pat = (
+        r"\[.*?\] \+\d+ \d{2} \d{7}:\s*(.*)"  # Unsaved Contact Seperator Pattern
+    )
+
+    saved_ctc_sep_pat = r"\[.*?\] .*?, .*?:\s*(.*)"  # Saved Contact Seperator Pattern
+    ctc_sep_pat = [
+        unsaved_ctc_sep_pat,
+        saved_ctc_sep_pat,
+    ]  # Merge between both patterns
+
+    extracted_message = []  # Emtpy list to store the actual messages
+
+    # For Loop done for each pattern saved and unsaved seperators
+    for pattern in ctc_sep_pat:
+        matches = re.findall(pattern, bulk)  # Put all matches in a list
+        for match in matches:
+            extracted_message.append(match)
+            # Stores all list elements in 'extracted_message' list
+    return extracted_message
