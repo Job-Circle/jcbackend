be362b765ceab3e8902787665c3d6a363de7b35e
Patrick Bassem Ramy <patrick.bassemrs@gmail.com>
2024-06-22 13:15:36 +0300

    Job-Circle/jcproduct#3 finalized parser

--- app/services/post_parser.py
+++ app/services/post_parser.py
@@ -4,29 +4,6 @@ import re
 import unicodedata
 
 
-def obtain_read_file():
-    global read_file_name
-    global downloads_path
-    # Get the path to the Downloads directory
-    downloads_path = os.path.join(os.path.expanduser("~"), "Downloads")
-
-    # Ask the user to input the filename
-    read_file_name = input("Enter the name of the file in the Downloads folder: ")
-
-    # Construct the full path to the file
-    file_path = os.path.join(downloads_path, read_file_name)
-
-    # Try to open the file and read its content
-    try:
-        with open(file_path, "r") as myfile:
-            content = myfile.read()
-        return content
-    except FileNotFoundError:
-        return f"File '{read_file_name}' not found in the Downloads folder."
-    except Exception as e:
-        return f"An error occurred: {e}"
-
-
 def extract_contact_info(text):
     """
     Extract phone numbers and emails from text.
@@ -92,8 +69,8 @@ def seperate_posts_from_whatsapp_bulk_text(input_string):
 
 
 # Calling the function
-def extract_post_full_info_from_whatsapp_bulk_text():
-    python_output = seperate_posts_from_whatsapp_bulk_text(obtain_read_file())
+def extract_post_full_info_from_whatsapp_bulk_text(bulk_txt):
+    python_output = seperate_posts_from_whatsapp_bulk_text(bulk_txt)
     result = []
     for entry in python_output:
         text_message = entry["Free Text"]
@@ -105,26 +82,3 @@ def extract_post_full_info_from_whatsapp_bulk_text():
     end_result = json.dumps(result, ensure_ascii=False, indent=2)
     end_result = end_result.replace("\\n", "\n")
     return end_result
-
-
-def write_parsed_messages_to_file():
-
-    content = extract_post_full_info_from_whatsapp_bulk_text()
-    # Define the filename
-    filename = f"{read_file_name}_parsed_messages.txt"
-
-    # Construct the full path to the file
-    file_path = os.path.join(downloads_path, filename)
-
-    # Call the extract_post_full_info_from_whatsapp_bulk_text function
-
-    # Try to write the content to the file
-    try:
-        with open(file_path, "w") as file:
-            file.write(content)
-        print(f"Content successfully written to {file_path}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
-write_parsed_messages_to_file()
