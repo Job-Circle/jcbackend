379289ec56394309f588aca69edcdac2b83eb4cb
Bassem Ramzy <sanpatbrs@gmail.com>
2024-07-06 16:46:58 +0300

    Extra validation for createPost

--- app/orchestrators/posts_orchestrator.py
+++ app/orchestrators/posts_orchestrator.py
@@ -4,14 +4,23 @@ from controllers.posts_controller import addPostToDB
 import json
 
 def createPost(request):
-    if request.method == 'POST':
+    try:
+        if request.method != 'POST':
+            return JsonResponse({'message': 'Invalid request method!'}, status=405)
+
+        # Validate inputs
+        if not request.body:
+            return JsonResponse({'message': 'Empty body!'}, status=400)
+
         post_data = json.loads(request.body.decode('utf-8'))
         failed = postToFacebook(post_data)
         if(not failed):
             addPostToDB(post_data)
         else:
             return JsonResponse({'message': 'Failed to create post!'}, status=500)
+        
         return JsonResponse({'message': 'Post created successfully!'})
-    else:
-        return JsonResponse({'message': 'Invalid request method!'}, status=405)
-
+        
+    except Exception as e:
+        self.output.info("Exception: " + str(e.__class__))
+        return None
